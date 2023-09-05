7cc6eb1e490f32ac3f57e3d93adf1cb781b6c773
pabrax <pabloeg0420@outlook.com>
2023-09-05 12:54:29 -0500

    tables.sql

---
+++ database.sql
@@ -0,0 +1,29 @@
+-- tira la tabla si existe
+
+USE master;
+ALTER DATABASE aerolinea SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+DROP DATABASE IF EXISTS aerolinea;
+GO
+
+CREATE DATABASE aerolinea
+ON PRIMARY
+(
+    NAME = 'aerolinea',
+    FILENAME = 'C:\mibase\data\aerolinea.mdf', -- Ruta del archivo de datos
+    SIZE = 50MB, -- Tamaño inicial del archivo de datos
+    FILEGROWTH = 25% -- Incremento de tamaño en porcentaje
+)
+LOG ON
+(
+    NAME = 'aerolinea_log',
+    FILENAME = 'C:\mibase\data\aerolinea.ldf', -- Ruta del archivo de registro de transacciones
+    SIZE = 50MB, -- Tamaño inicial del archivo de registro de transacciones
+    FILEGROWTH = 25% -- Incremento de tamaño en porcentaje
+);
+
+GO
+
+
+ALTER DATABASE aerolinea
+SET COMPATIBILITY_LEVEL = 110;  
+GO
---
+++ tables.sql
@@ -0,0 +1,6 @@
+USE aerolinea;
+GO
+
+CREATE TABLE name(
+    id_name INT IDENTITY(1, 1) PRIMARY KEY
+);
