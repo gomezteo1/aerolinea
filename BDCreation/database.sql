049c41aafde18fe72fc82ba3b7309af4274a3668
pabrax <pabloeg0420@outlook.com>
2023-09-20 07:21:04 -0500

    punto 9

--- BDCreation/database.sql
+++ BDCreation/database.sql
@@ -20,6 +20,28 @@ LOG ON
     FILEGROWTH = 25% -- Incremento de tamaño en porcentaje
 );
 
+-- CREATE DATABASE aerolinea
+-- ON PRIMARY
+-- (
+--     NAME = 'aerolinea',
+--     FILENAME = '/var/opt/mssql/data/aerolinea.mdf', -- Ruta del archivo de datos en el contenedor
+--     SIZE = 50MB, -- Tamaño inicial del archivo de datos
+--     FILEGROWTH = 25% -- Incremento de tamaño en porcentaje
+-- )
+-- LOG ON
+-- (
+--     NAME = 'aerolinea_log',
+--     FILENAME = '/var/opt/mssql/data/aerolinea.ldf', -- Ruta del archivo de registro de transacciones en el contenedor
+--     SIZE = 50MB, -- Tamaño inicial del archivo de registro de transacciones
+--     FILEGROWTH = 25% -- Incremento de tamaño en porcentaje
+-- );
+GO
+
+-- Establece el nivel de compatibilidad de la base de datos
+ALTER DATABASE aerolinea
+SET COMPATIBILITY_LEVEL = 110;  
+GO
+
 GO
 
 
