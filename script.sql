f4aff4e946d634ab4d81680a002c28f5ba69a119
mateo <atehortuamateo123@gmail.com>
2023-10-10 08:16:12 -0500

    tiene el backup

--- script.sql
+++ script.sql
@@ -4,38 +4,38 @@ ALTER DATABASE aerolinea SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
 DROP DATABASE IF EXISTS aerolinea;
 GO
 
--- CREATE DATABASE aerolinea
--- ON PRIMARY
--- (
---     NAME = 'aerolinea',
---     FILENAME = 'C:\mibase\data\aerolinea.mdf', -- Ruta del archivo de datos
---     SIZE = 50MB, -- Tamaño inicial del archivo de datos
---     FILEGROWTH = 25% -- Incremento de tamaño en porcentaje
--- )
--- LOG ON
--- (
---     NAME = 'aerolinea_log',
---     FILENAME = 'C:\mibase\data\aerolinea.ldf', -- Ruta del archivo de registro de transacciones
---     SIZE = 50MB, -- Tamaño inicial del archivo de registro de transacciones
---     FILEGROWTH = 25% -- Incremento de tamaño en porcentaje
--- );
+ CREATE DATABASE aerolinea
+ ON PRIMARY
+ (
+     NAME = 'aerolinea',
+     FILENAME = 'C:\mibase\data\aerolinea.mdf', -- Ruta del archivo de datos
+     SIZE = 50MB, -- Tamaño inicial del archivo de datos
+     FILEGROWTH = 25% -- Incremento de tamaño en porcentaje
+ )
+ LOG ON
+ (
+     NAME = 'aerolinea_log',
+     FILENAME = 'C:\mibase\data\aerolinea.ldf', -- Ruta del archivo de registro de transacciones
+     SIZE = 50MB, -- Tamaño inicial del archivo de registro de transacciones
+     FILEGROWTH = 25% -- Incremento de tamaño en porcentaje
+ );
 
-CREATE DATABASE aerolinea
-ON PRIMARY
-(
-    NAME = 'aerolinea',
-    FILENAME = '/var/opt/mssql/data/aerolinea.mdf', -- Ruta del archivo de datos en el contenedor
-    SIZE = 50MB, -- Tamaño inicial del archivo de datos
-    FILEGROWTH = 25% -- Incremento de tamaño en porcentaje
-)
-LOG ON
-(
-    NAME = 'aerolinea_log',
-    FILENAME = '/var/opt/mssql/data/aerolinea.ldf', -- Ruta del archivo de registro de transacciones en el contenedor
-    SIZE = 50MB, -- Tamaño inicial del archivo de registro de transacciones
-    FILEGROWTH = 25% -- Incremento de tamaño en porcentaje
-);
-GO
+--CREATE DATABASE aerolinea
+--ON PRIMARY
+--(
+--    NAME = 'aerolinea',
+--    FILENAME = '/var/opt/mssql/data/aerolinea.mdf', -- Ruta del archivo de datos en el contenedor
+--    SIZE = 50MB, -- Tamaño inicial del archivo de datos
+--    FILEGROWTH = 25% -- Incremento de tamaño en porcentaje
+--)
+--LOG ON
+--(
+--    NAME = 'aerolinea_log',
+--    FILENAME = '/var/opt/mssql/data/aerolinea.ldf', -- Ruta del archivo de registro de transacciones en el contenedor
+--    SIZE = 50MB, -- Tamaño inicial del archivo de registro de transacciones
+--    FILEGROWTH = 25% -- Incremento de tamaño en porcentaje
+--);
+--GO
 
 -- Establece el nivel de compatibilidad de la base de datos
 ALTER DATABASE aerolinea
@@ -980,7 +980,13 @@ VALUES (109, 9, 9, 9, 9, 10, '2024-10-18 20:00:00', '2023-10-18 22:30:00', 150, 
 INSERT INTO vuelo (no_vuelo, id_tripulacion, id_reserva, id_avion, id_destino_origen, id_destino_llegada, fecha_hora_salida, fecha_hora_llegada, duracion_vuelo, id_estado_vuelo, id_tipo_vuelo)
 VALUES (110, 10, 10, 10, 10, 1, '2024-10-19 10:00:00', '2023-10-19 12:15:00', 135, 3, 3);
 
+---Backup de seguridad
 
+--completo
+backup database aerolinea to disk='C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\Backup\aerolinea.bak'
+
+--diferencial
+backup database aerolinea to disk='C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\Backup\aerolinea.bak' with differential
 
 
 
